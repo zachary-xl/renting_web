dbb07d8b7562fe6b0029682c9dd3c63cbee8b091
liuxilong <liuxilong@88.com>
2023-05-18 09:18:55 +0800

    add:新增hook

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,1 +1,10 @@
-import { storeToRefs } from 'pinia'
+import { storeToRefs } from "pinia";
+import { useAppStore } from "@/store";
+
+export function useAppStoreToRefs() {
+  const appStore = useAppStore();
+  return {
+    appStore,
+    ...storeToRefs(appStore)
+  };
+}
